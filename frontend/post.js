0bb6a9a155508f3d4cdbc555f0060a18d621e78b
Maheshkumar Bapurao Nale <97023214+maheshNALE@users.noreply.github.com>
2023-05-24 18:46:08 +0530

    Add files via upload

---
+++ frontend/post.js
@@ -0,0 +1,28 @@
+{/* <script>
+    const clickme = ()=>{
+     const payload = {
+        title:document.getElementById("title").value,
+        body:document.getElementById("body").value,
+        no_if_comments:document.getElementById("no_if_comments").value,
+        
+        
+     }
+
+
+  
+ 
+     fetch("https://blushing-helmet-clam.cyclic.app/post/create",{
+         method:"POST",
+         headers:{"content-type":"application/json"},
+         body:JSON.stringify(payload)
+     
+     }).then(res=> res.json())
+     .then(res=>
+     
+     console.log(res))
+     .catch(res=>console.log(res))
+ 
+ 
+    }
+ 
+ </script> */}
